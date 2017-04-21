3c16b8823cb83067ba631448f182915861c1f6a8
hujinqiang <854024269@qq.com>
2017-04-21 17:27:44 +0800

    总积分计算方法提取

--- RefactorTest/src/com/tocel/refactor/Customer.java
+++ RefactorTest/src/com/tocel/refactor/Customer.java
@@ -18,31 +18,43 @@ public class Customer {
 	}
 	
 	public String statement(){
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		
 		while(rentals.hasMoreElements()){
 			Rental each = rentals.nextElement();
 			
-			frequentRenterPoints += each.getFrequentRenterPoints();
-			
-			
 			//show figures for this rental
 			result += "\t" + each.get_movie().get_title() + "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
-			
 		}
 		
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " requent renter points";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " requent renter points";
 		
 		return result;
 		
+		
 	}
 
+	private double getTotalCharge(){
+		double result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while(rentals.hasMoreElements()){
+			Rental each = rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
 	
+	private int getTotalFrequentRenterPoints(){
+		int result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while(rentals.hasMoreElements()){
+			Rental each = rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
 	
 }
